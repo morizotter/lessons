7a96685cebf62911e008c9186fa03fce18d73535
Naoki Morita <namorit@gmail.com>
2014-05-03 23:41:23 +0900

    add node express basic environment

--- RAYWENDERLICH/NodeTutorial/index.js
+++ RAYWENDERLICH/NodeTutorial/index.js
@@ -1,11 +1,24 @@
 var http = require('http'),
-express = require('express');
+express = require('express'),
+path = require('path');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
 	res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+app.get('/:a?/:b?/:c?', function (req,res) {
+	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+});
+
+app.use(function (req, res) {
+	res.render('404', {url:req.url});
 });
 
 http.createServer(app).listen(app.get('port'), function() {
